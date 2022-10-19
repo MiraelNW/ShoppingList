1b10d32739100c45cfc788abdaf27eee4fdab029
M1rael3 <goodworkinspclass@mail.ru>
2022-10-19 21:18:31 +0700

    start for clean architecture

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel : MainViewModel
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("Main", it.toString())
+            if (count == 0) {
+                val item = it[0]
+                viewModel.changeEnableState(item)
+                count++
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.data.ShopListRepositoryImpl.getShopList
+import com.example.shoppinglist.domain.*
+
+class MainViewModel : ViewModel() {
+    private val repository = ShopListRepositoryImpl
+
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+
+
+}
